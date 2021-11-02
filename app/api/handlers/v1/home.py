e2290cd24ec4899028752f26753784cc26a0ef1e
Abu Shumon <abushumon@AbuMacBookProQvik.local>
2021-11-02 18:16:03 +0000

    logger updated

--- app/api/handlers/v1/home.py
+++ app/api/handlers/v1/home.py
@@ -1,11 +1,8 @@
 """Home handler module"""
 
-import logging
-
 from typing import Any
 from fastapi import APIRouter
 
-logger = logging.getLogger(__name__)
 router = APIRouter()
 
 
--- app/server.py
+++ app/server.py
@@ -1,6 +1,6 @@
 """Application server module"""
 
-import logging
+# import logging
 import uvicorn
 
 from fastapi import FastAPI
@@ -8,11 +8,26 @@ from fastapi import FastAPI
 from app.core import settings
 from app.api.handlers.v1 import api_router
 
-logger = logging.getLogger(__name__)
+log_config = uvicorn.config.LOGGING_CONFIG
 
 app = FastAPI(title=settings.PROJECT_NAME)
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
 
 def init_server() -> None:
-    uvicorn.run("app.server:app", host="0.0.0.0", port=9000, reload=True)
+    """
+    Configure logging and log format.
+    Spin up the server.
+    """
+    # Log format
+    log_config["formatters"]["access"]["fmt"] = "[%(asctime)s.%(msecs)d] | %(levelname)s | [%(name)s:%(filename)s:%(lineno)d] - %(message)s"
+    log_config["formatters"]["default"]["fmt"] = "[%(asctime)s.%(msecs)d] | %(levelname)s | [%(name)s:%(filename)s:%(lineno)d] - %(message)s"
+
+    # Date format
+    date_fmt = "%Y-%m-%d:%H:%M:%S"
+    print("date_fmt", date_fmt)
+    log_config["formatters"]["default"]["datefmt"] = date_fmt
+    log_config["formatters"]["access"]["datefmt"] = date_fmt
+
+    # Run the server
+    uvicorn.run("app.server:app", log_config=log_config, host="0.0.0.0", port=9000, reload=True)
